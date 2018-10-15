6265b31638f2c69ebea0af257ed490c974b24ddb
yatochk <yatochk@github.com>
2018-10-15 21:16:10 +0400

    Revert "First commit"
    This reverts commit d1ffc318722a284d54ab61d9e2583d3b94339de9.

--- app/src/main/java/com/hhade/loans/Offers.kt
+++
@@ -1,4 +0,0 @@
-package com.hhade.loans
-
-class Offers(val name: String, val rating: Float, var sum: String, val time: String, val rate: String,
-             val url: String, val descriptionMap: LinkedHashMap<String, String>, val imageUrl: String)
